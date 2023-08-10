682cdc865b8bcb1dce1263aeeda80abdb05eebe6
chininha-03 <tatazinhoow@gmail.com>
2023-08-10 17:11:38 -0300

    Finalizando aul 10/08

---
+++ src/components/profile/profile.jsx
@@ -0,0 +1,8 @@
+export default function profile() {
+    return(
+        <div className="profile">
+            <img src="" alt="" />
+            <p>@o_chininha</p>
+        </div>
+    )
+};
---
+++ src/pages/App.jsx
@@ -0,0 +1,10 @@
+ import "./App.css"
+ import Profile from "../components/profile/profile"
+
+ export default function App () {
+    return(
+        <>
+        <Profile></Profile>
+        </>
+    )
+ }
